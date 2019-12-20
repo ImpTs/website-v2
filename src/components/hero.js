7f6c4838ad7b94d31aa393b18d4a4851f88a079d
impT <cosmokid3@gmail.com>
2019-12-20 16:46:14 -0600

    2.0 ready! :)

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,30 +4,20 @@ import Img from "./image-city.js"
 
 const Hero =({siteTitle}) => (
 
-    <div  className="section">
-      <div className="columns">
-      <div className="column">
-      <div className="container is-center">
-        <figure className=""></figure>
-        <h2 className="title">Seth Flagg</h2>
-        <h2 className="subtitle">
-          Front end web developer
-        </h2><Img className="image-is-square"
-        fluid="data.file.childImageSharp.fluid"
-        alt=" city by the lake"
-        style={{
-          position: "absolute",
-          left: 0,
-          top: 0,
-          width: "100%",
-          height: "100%"
-        }} />
-        <p>Making responsive, mobile-first webpages with modern web technologies so that you can make the best impression.</p>
-        </div>
-        </div>
-      </div>
-      </div>
-   
+  <section class="hero hero-image is-primary is-medium">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">
+        Seth Flagg
+      </h1>
+      <h2 class="subtitle">
+        Front End Web Developer
+      </h2>
+    </div>
+  </div>
+</section>
+  
+
       
 )
 export default Hero
